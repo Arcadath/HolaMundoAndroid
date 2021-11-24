71ed497ace8c203f8b8bf03418be60db2e274daa
Arcadath <yaelemanuelperez14@gmail.com>
2021-11-23 22:37:58 -0600

    Test

---
+++ app/src/main/java/ipn/cecyt9/HolaMundoAndroid/MainActivity.java
@@ -0,0 +1,101 @@
+package ipn.cecyt9.HolaMundoAndroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void suma(View miView)
+    { }
+    public void resta(View miView)
+    { }
+    public void multiplicacion(View miView)
+    { }
+    public void division(View miView)
+    { }
+    public void MetrosPies(View miView)
+    { }
+
+    private Button Button_suma;
+    private Button Button_resta;
+    private Button Button_division;
+    private Button Button_multiplicacion;
+    private Button ButtonPesosDolar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button_suma = findViewById(R.id.Button_suma);
+        Button_resta = findViewById(R.id.Button_resta);
+        Button_multiplicacion = findViewById(R.id.Button_multiplicacion);
+        Button_division = findViewById(R.id.Button_division);
+        ButtonPesosDolar = findViewById(R.id.ButtonPesosDolar);
+
+        Button_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirSuma();
+            }
+        });
+
+        Button_resta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirResta();
+            }
+        });
+
+        Button_multiplicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirMultiplicacion();
+            }
+        });
+
+        Button_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirDivision();
+            }
+        });
+
+        ButtonPesosDolar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirMetrosPies();
+            }
+        });
+
+    }
+
+    private void abrirSuma() {
+        Intent intent = new Intent(this , SumaActivity.class);
+    startActivity(intent);
+    }
+
+    private void abrirResta() {
+        Intent intent = new Intent(this , restaActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirMultiplicacion() {
+        Intent intent = new Intent(this , multiplicacionActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirDivision() {
+        Intent intent = new Intent(this , divisionActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirMetrosPies() {
+        Intent intent = new Intent(this , MetrosPies.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/ipn/cecyt9/HolaMundoAndroid/MetrosPies.java
@@ -0,0 +1,84 @@
+package ipn.cecyt9.HolaMundoAndroid;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import static android.widget.Toast.LENGTH_SHORT;
+import android.widget.Toast;
+public class MetrosPies extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metros_pies);
+
+    }
+    Double resultado;
+    Double resultado2;
+    Double numero1;
+    Double numero2;
+
+
+
+
+    public void onClickOperacionCapturaNumero1(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.numeros) ;
+        numero1 = Double.parseDouble(tv.getText().toString());
+        tv.setText("");
+    }
+
+    public void onClicksuma(View miView)
+    {
+        onClickOperacionCapturaNumero1(miView);
+        TextView tv = (TextView) findViewById(R.id.numeros) ;
+
+        try {
+            resultado = numero1 * 3.2808;
+
+            tv.setText(resultado.toString());
+        }catch(NumberFormatException nfe){
+            Toast.makeText(this,"Numero Incorrecto", LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void onClickButtonborrar(View miView)
+    {
+        numero1=0.0;
+        TextView tv = (TextView) findViewById(R.id.numeros) ;
+        tv.setText("");
+    }
+
+
+    public void onClickOperacionCapturaNumero2(View miView)
+    {
+        TextView tv = (TextView) findViewById(R.id.numeros2) ;
+        numero2 = Double.parseDouble(tv.getText().toString());
+        tv.setText("");
+    }
+
+
+    public void onClicksuma2(View miView)
+    {
+        onClickOperacionCapturaNumero2(miView);
+        TextView tv = (TextView) findViewById(R.id.numeros2) ;
+
+        try {
+            resultado2 = numero2 / 3.2808;
+
+            tv.setText(resultado2.toString());
+        }catch(NumberFormatException nfe){
+            Toast.makeText(this,"Numero Incorrecto", LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void onClickButtonborrar2(View miView)
+    {
+        numero2=0.0;
+        TextView tv = (TextView) findViewById(R.id.numeros2) ;
+        tv.setText("");
+    }
+
+}
